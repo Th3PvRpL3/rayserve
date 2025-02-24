d8057da774560694b26937894061a5a45437308b
Th3PvRpL3 <nicolasperezleis@gmail.com>
2025-02-24 09:42:49 +0100

    Update rayserve_health_check.py

--- rayserve_health_check.py
+++ rayserve_health_check.py
@@ -1,9 +1,5 @@
-import io
-import base64
-import torch
 from ray import serve
 import ray
-from diffusers import StableDiffusionPipeline
 
 class HealthCheck:
     async def __call__(self, request):
